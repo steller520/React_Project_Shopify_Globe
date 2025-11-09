a49ada3fa7d5d90b3587ee1ba45290e69562ca98
steller520 <shivamnegi571@gmail.com>
2025-11-09 12:35:51 +0530

    feat: update Header component to improve dropdown interaction and navigation styling

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -15,29 +15,30 @@ function Header() {
     
     return (
         <>
-            <header id='header' className={`flex ${dropdownVisible ? 'mb-20 md:mb-auto' : ''}  sticky shadow-white/10 top-0 z-10 md:justify-between  items-center p-4 h-18 shadow-md text-xl font-bold bg-linear-to-r from-yellow-800 via-amber-700 to-yellow-900 w-full text-white`}>
+            <header id='header' className={`flex ${dropdownVisible ? 'mb-20 md:mb-auto' : ''} w-full sticky shadow-white/10  top-0 z-10 md:justify-between  items-center p-4 h-18 shadow-md text-xl font-bold bg-linear-to-r from-yellow-800 via-amber-700 to-yellow-900  text-white`}>
                 {/* Logo and Title */}
                 <div className='md:ml-4 w-full  flex justify-between md:justify-normal items-center gap-4'>
-                    <img onMouseOver={handleDropDown} src="/src/assets/shopifyglobe.svg" alt="Shopify Globe" className='md:w-10 md:h-10 w-12 h-12' />
+                    <img onClick={handleDropDown} src="/src/assets/shopifyglobe.svg" alt="Shopify Globe" className='md:w-10 md:h-10 w-12 h-12' />
                     <h1 className='text-2xl font-bold'>Shopify Globe</h1>
                 </div>
-                <nav>
+                <nav >
                     {
-                        dropdownVisible ? (
-                            <ul className='absolute border w-full top-18 left-0 bg-linear-to-r from-yellow-800 via-amber-700 to-yellow-900 md:bg-inherit md:static gap-8 md:mr-4 md:flex'>
+                        dropdownVisible && (
+                            <ul className='absolute border flex flex-col items-center justify-center w-full p-3 top-18 left-0 bg-linear-to-r from-yellow-800 via-amber-700 to-yellow-900 md:bg-inherit md:static md:hidden gap-2 md:mr-4 '>
                                 {/* Navigation links */}
-                                <li><Link to="/" className='hover:text-yellow-300 transition-colors flex border-b justify-center md:justify-normal items-center gap-2'><TiHomeOutline className='inline-block mr-1 md:w-8 md:h-8' />Home</Link></li>
+                                <li><Link to="/" className='hover:text-yellow-300 transition-colors flex  justify-center md:justify-normal items-center gap-2'><TiHomeOutline className='inline-block mr-1 md:w-8 md:h-8' />Home</Link></li> 
+                                <div className='bg-white w-full h-0.5'></div>
                                 <li><Link to="/cart" className='hover:text-yellow-300 transition-colors justify-center md:justify-normal flex items-center gap-2'><IoCartOutline className='inline-block mr-1 md:w-8 md:h-8' />Cart({cartItems.length})</Link></li>
                             </ul>
-                        ): (
+                        ) 
 
+                        
+                    }
                             <ul className='hidden md:flex md:bg-inherit gap-8 md:mr-4'>
                                 {/* Navigation links */}
                                 <li><Link to="/" className='hover:text-yellow-300 transition-colors flex items-center gap-2'><TiHomeOutline className='inline-block mr-1 md:w-8 md:h-8' />Home</Link></li>
                                 <li><Link to="/cart" className='hover:text-yellow-300 transition-colors flex items-center gap-2'><IoCartOutline className='inline-block mr-1 md:w-8 md:h-8' />Cart({cartItems.length})</Link></li>
                             </ul>
-                        )
-                    }
                 </nav>
 
             </header>
