510f7b3aaaa80760970f4c3d4175e6a38f5e079a
steller520 <shivamnegi571@gmail.com>
2025-11-09 10:24:20 +0530

    feat: refactor ProductList component layout and improve search input structure

--- src/component/ProductList.jsx
+++ src/component/ProductList.jsx
@@ -40,7 +40,7 @@ function ProductList() {
 
     return (
         <div>
-            {<div className='flex flex-wrap lg:flex-nowrap md:gap-6 gap-4 mt-10 mb-4 mx-auto justify-center max-w-7xl'>
+            <div className='flex flex-wrap lg:flex-nowrap md:gap-6 gap-4 mt-10 mb-4 mx-auto justify-center max-w-7xl'>
                 {categories.map((category, index) => (
                     <button key={index} value={category.toLowerCase()} onClick={(e) => handleClick(e)} className='flex justify-center items-center border bg-linear-to-r from-yellow-700 to-red-700 text-white shadow-lg border-blue-600 lg:w-64 lg:h-18 w-32 h-12 rounded-lg hover:bg-blue-600 transition-all'>
                         {category.toUpperCase()}
@@ -49,8 +49,7 @@ function ProductList() {
                 <button onClick={() => setFilteredProducts(products)} className='flex justify-center items-center border bg-linear-to-r from-yellow-700 to-red-700 text-white shadow-lg border-blue-600 lg:w-64 lg:h-18 w-32 h-12 rounded-lg hover:bg-blue-600 transition-all'>
                     Reset
                 </button>
-            </div>  
-            }
+            </div>
             <div className='flex items-center justify-between p-8 bg-blue-100 max-w-7xl mx-auto border border-blue-300 rounded-lg h-10 shadow-md'>
                 <h1 className='ml-4 text-3xl text-blue-800 font-semibold ' >Search</h1>
                 <div className='flex justify-between items-center w-auto gap-1.5'>
@@ -58,13 +57,13 @@ function ProductList() {
                     <input onChange={(e) => {handleSearch(e.target.value)}} className='border border-gray-300 rounded-lg px-4 py-2 w-64 focus:outline-none focus:ring-2 focus:ring-blue-400 bg-white' type="text" placeholder='Search products...' />
                 </div>
             </div>
-            <div className='grid lg:grid-cols-4  sm:grid-cols-2 grid-cols-1 gap-4 max-w-7xl mx-auto'>
+            <ul className='grid lg:grid-cols-4  sm:grid-cols-2 grid-cols-1 gap-4 max-w-7xl mx-auto list-none'>
                 {filteredProducts.map(product => (
                     <li key={product.id}>
                         <MemoizedProductItem product={product} />
                     </li>
                 ))}
-            </div>
+            </ul>
         </div>
     )
 }
